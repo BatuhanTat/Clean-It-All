95c9a26dfb29010a5e277fad9954be0fa843ccb8
Batuhan TATLISERT <90261616+Batuhugu@users.noreply.github.com>
2023-08-17 21:36:04 +0300

    V0.75
    - UI Button Level Selection implemented. When cliked to the level button, related level will be loaded.
    - Level Complete particle effect created. It is star effect.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] GameObject levelPanel;
 
 
+    float levelLoadDelay = 1f;
+    string sceneName;
 
     public void LevelPanel()
     {
@@ -17,5 +21,21 @@ public class UIManager : MonoBehaviour
     public void SettingsPanel()
     {
         Debug.Log("Setttings worked");
+    }
+
+    public void SelectLevel(Button button)
+    {
+        // Get the name of the clicked button's GameObject
+        sceneName = button.name;
+
+        // Do something with the buttonName, such as printing it
+        Debug.Log("Clicked button name: " + sceneName);
+        Invoke(nameof(LoadDelayedScene), levelLoadDelay);
+        LevelPanel();
+    }
+
+    private void LoadDelayedScene()
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
